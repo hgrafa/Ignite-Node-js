a03052cfc572bfa9efff0c0d954d645798e6d327
hgrafa <hugorafell@gmail.com>
2023-06-24 20:21:27 -0300

    adding methods and stateful sample

--- 01-fundamentos/src/server.js
+++ 01-fundamentos/src/server.js
@@ -1,17 +1,27 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("usuarios");
+    return res
+      .setHeader("Content-Type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    return res.end("usuarios");
+    users.push({
+      name: "John",
+      age: 22,
+      height: 1.8,
+    });
+
+    return res.end("cadastrado");
   }
 
-  return res.end("Hello world!");
+  return res.end("rota desconhecida");
 });
 
 server.listen(3333);
