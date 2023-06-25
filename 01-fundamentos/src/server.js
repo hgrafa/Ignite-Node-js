7d56a47f3e08cee0f5cab5f3056b2aefb6e22235
hgrafa <hugorafell@gmail.com>
2023-06-24 21:55:47 -0300

    persisting users

--- 01-fundamentos/src/server.js
+++ 01-fundamentos/src/server.js
@@ -1,27 +1,32 @@
 import http from "node:http";
+import { randomUUID } from "node:crypto";
+import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
 
-const users = [];
+const database = new Database();
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
+  await json(req, res);
+
   if (method === "GET" && url === "/users") {
+    const users = database.select("users");
+
     return res
       .setHeader("Content-Type", "application/json")
       .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    users.push({
-      name: "John",
-      age: 22,
-      height: 1.8,
-    });
+    const { name, age, height } = req.body;
+    const user = { id: randomUUID(), name, age, height };
 
+    database.insert("users", user);
     return res.writeHead(201).end("cadastrado");
   }
 
-  return res.writeHead(404).end("rota desconhecida");
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
