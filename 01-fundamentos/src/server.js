8695531b526d0e52a208fdf7c41cb1fec7c49e0f
hgrafa <hugorafell@gmail.com>
2023-06-25 00:33:41 -0300

    implementing regex url to use path variables

--- 01-fundamentos/src/server.js
+++ 01-fundamentos/src/server.js
@@ -1,29 +1,16 @@
 import http from "node:http";
-import { randomUUID } from "node:crypto";
 import { json } from "./middlewares/json.js";
-import { Database } from "./database.js";
-
-const database = new Database();
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
-
   await json(req, res);
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
-
-    return res
-      .setHeader("Content-Type", "application/json")
-      .end(JSON.stringify(users));
-  }
-
-  if (method === "POST" && url === "/users") {
-    const { name, age, height } = req.body;
-    const user = { id: randomUUID(), name, age, height };
-
-    database.insert("users", user);
-    return res.writeHead(201).end("cadastrado");
+  if (route) {
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end();
